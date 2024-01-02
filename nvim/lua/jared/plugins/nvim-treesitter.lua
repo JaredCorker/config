50c900e64957a1732910c70627d3296599abdb07
Jared Corker <jared.corker@gmail.com>
2024-01-02 15:34:07 +1100

    add graphql to treesitter ensure_installed

--- nvim/lua/jared/plugins/nvim-treesitter.lua
+++ nvim/lua/jared/plugins/nvim-treesitter.lua
@@ -5,7 +5,7 @@ return {
     local configs = require("nvim-treesitter.configs")
 
     configs.setup({
-      ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "typescript", "html" },
+      ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "typescript", "html", "graphql" },
       sync_install = false,
       highlight = { enable = true },
       indent = { enable = true },
