38407fa798e733a5c2e0788defc285f3483c49fc
Jared Corker <jared.corker@gmail.com>
2024-01-04 12:01:16 +1100

    add git-blame

---
+++ nvim/lua/jared/plugins/git-blame.lua
@@ -0,0 +1,8 @@
+return {
+  "f-person/git-blame.nvim",
+  opts = {
+    date_format = "%r",
+    message_when_not_committed = "Uncommited",
+    message_template = "<author> • <date> • <summary>",
+  },
+}
