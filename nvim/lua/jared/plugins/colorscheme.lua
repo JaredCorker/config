1760259445291091fd2e42cf1593066589bfd423
Jared Corker <jared.corker@gmail.com>
2024-01-05 16:06:28 +1100

    add Gzb`` keymap

--- nvim/lua/jared/plugins/colorscheme.lua
+++ nvim/lua/jared/plugins/colorscheme.lua
@@ -3,6 +3,9 @@ return {
   lazy = false,
   priority = 1000,
   config = function()
+    require('tokyonight').setup({
+      transparent = true
+    })
     vim.cmd([[colorscheme tokyonight]])
   end,
 }
