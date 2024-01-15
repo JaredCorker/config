0a0ed37f16fd19413f131726fe1727c6fbe5e8d8
Jared Corker <jared.corker@gmail.com>
2024-01-15 22:38:53 +1100

    lazy load oil

--- nvim/lua/jared/plugins/oil.lua
+++ nvim/lua/jared/plugins/oil.lua
@@ -1,12 +1,12 @@
 return {
   "stevearc/oil.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    require("oil").setup({
-      view_options = {
-        show_hidden = true,
-      },
-    })
-    vim.keymap.set("n", "-", ":Oil<CR>")
-  end,
+  keys = {
+    { "-", ":Oil<CR>" },
+  },
+  opts = {
+    view_options = {
+      show_hidden = true,
+    },
+  },
 }
