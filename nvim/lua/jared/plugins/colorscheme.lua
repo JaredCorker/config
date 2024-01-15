2b1e417146bdf1ce57091eceb071f0ce902be62e
Jared Corker <jared.corker@gmail.com>
2024-01-15 22:39:05 +1100

    replace tokyonight with catppuccin

--- nvim/lua/jared/plugins/colorscheme.lua
+++ nvim/lua/jared/plugins/colorscheme.lua
@@ -1,11 +1,10 @@
 return {
-  "folke/tokyonight.nvim",
+  "catppuccin/nvim",
+  name = "catppuccin",
   lazy = false,
   priority = 1000,
   config = function()
-    require('tokyonight').setup({
-      transparent = true
-    })
-    vim.cmd([[colorscheme tokyonight]])
+    require("catppuccin").setup({})
+    vim.cmd.colorscheme("catppuccin")
   end,
 }
