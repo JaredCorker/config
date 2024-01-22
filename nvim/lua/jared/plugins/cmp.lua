9b3e907f93873c4287e84b5e29d40e0957304da1
Jared Corker <jared.corker@gmail.com>
2024-01-23 09:38:28 +1100

    change cmp complete keymap due to tmux conflict

--- nvim/lua/jared/plugins/cmp.lua
+++ nvim/lua/jared/plugins/cmp.lua
@@ -29,7 +29,7 @@ return {
         mapping = cmp.mapping.preset.insert({
           ["<C-d>"] = cmp.mapping.scroll_docs(-4),
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
-          ["<C-Space>"] = cmp.mapping.complete({}),
+          ["<C-h>"] = cmp.mapping.complete({}),
           ["<CR>"] = cmp.mapping.confirm({
             behavior = cmp.ConfirmBehavior.Replace,
             select = true,
