ff49168c1047893d16519d0a3e5d5dd2bb64a961
Jared Corker <jared.corker@gmail.com>
2024-02-02 15:15:35 +1100

    add cmp path

--- nvim/lua/jared/plugins/cmp.lua
+++ nvim/lua/jared/plugins/cmp.lua
@@ -7,6 +7,7 @@ return {
     dependencies = {
       "saadparwaiz1/cmp_luasnip",
       "rafamadriz/friendly-snippets",
+      "hrsh7th/cmp-path",
     },
   },
   {
@@ -38,6 +39,7 @@ return {
         sources = {
           { name = "nvim_lsp" },
           { name = "luasnip" },
+          { name = "path" },
         },
       })
     end,
