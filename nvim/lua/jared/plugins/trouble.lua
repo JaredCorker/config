222f4920ad4efb786837ec2b808205055678eb2d
Jared Corker <jared.corker@gmail.com>
2024-02-02 11:23:06 +1100

    add trouble keymap

--- nvim/lua/jared/plugins/trouble.lua
+++ nvim/lua/jared/plugins/trouble.lua
@@ -2,4 +2,7 @@ return {
   "folke/trouble.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   opts = {},
+  keys = {
+    { "<leader>tt", ":Trouble<CR>", desc = "Open trouble" },
+  },
 }
