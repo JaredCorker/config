bfcb404cc044c2dd21575406e4b1a66313f80f56
Jared Corker <jared.corker@gmail.com>
2024-05-28 14:32:48 +1000

    add dir-telescope extension

---
+++ nvim/lua/jared/plugins/dir-telescope.lua
@@ -0,0 +1,17 @@
+return {
+  "princejoogie/dir-telescope.nvim",
+  -- telescope.nvim is a required dependency
+  requires = { "nvim-telescope/telescope.nvim" },
+  config = function()
+    require("dir-telescope").setup({
+      -- these are the default options set
+      hidden = true,
+      no_ignore = false,
+      show_preview = true,
+    })
+    require("telescope").load_extension("dir")
+
+    vim.keymap.set("n", "<leader>fdf", "<cmd>FileInDirectory<CR>", { desc = "Find files in dir" })
+    vim.keymap.set("n", "<leader>fds", "<cmd>GrepInDirectory<CR>", { desc = "Find string in dir" })
+  end,
+}
