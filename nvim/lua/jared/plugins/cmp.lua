89bc213a3fb55a36a714a40f53759e2ce04347ad
Jared Corker <jared.corker@gmail.com>
2024-06-02 23:02:32 +1000

    cmp change confirm behviour from replace to insert

--- nvim/lua/jared/plugins/cmp.lua
+++ nvim/lua/jared/plugins/cmp.lua
@@ -32,7 +32,7 @@ return {
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
           ["<C-h>"] = cmp.mapping.complete({}),
           ["<CR>"] = cmp.mapping.confirm({
-            behavior = cmp.ConfirmBehavior.Replace,
+            behavior = cmp.ConfirmBehavior.Insert,
             select = true,
           }),
         }),
