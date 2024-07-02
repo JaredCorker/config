3fde0b8615805f325792058addecba8805237918
Jared Corker <jared.corker@gmail.com>
2024-07-02 14:11:55 +1000

    add toggleterm

---
+++ nvim/lua/jared/plugins/toggleterm.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    config = function()
+      require("toggleterm").setup({
+        size = function(term)
+          if term.direction == "horizontal" then
+            return 15
+          elseif term.direction == "vertical" then
+            return vim.o.columns * 0.4
+          end
+        end,
+        open_mapping = [[<c-\>]],
+        direction = "vertical",
+      })
+    end,
+  },
+}
